7302fa302057a462a8fe86acab3a541c9c3b1888
mohammed EDDAHMANI <dsimo4323@gmail.com>
2024-07-29 22:22:06 +0200

    jtf

--- commerce-sw/app/api/protected-route.js
+++
@@ -1,8 +0,0 @@
-// pages/api/protected-route.js
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-
-const handler = (req, res) => {
-    res.status(200).json({ message: "This is a protected route" });
-};
-
-export default withClerkMiddleware(handler);
