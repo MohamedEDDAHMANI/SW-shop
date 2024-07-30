3aad22c7ea0bdc4f059475032b7635c85a45e22e
mohammed EDDAHMANI <dsimo4323@gmail.com>
2024-07-30 13:35:04 +0200

    add.

--- commerce-sw/middleware.js
+++ commerce-sw/middleware.js
@@ -1,7 +1,12 @@
-export function customMiddleware(req, res, next) {
-  // Your custom authentication logic here
-  if (req.headers.authorization === 'your-expected-auth-header') {
-      return next();
-  }
-  return res.status(401).json({ message: 'Unauthorized' });
-}
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
