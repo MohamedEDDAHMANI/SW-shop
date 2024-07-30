f3a4e262dea2e4e18954bfbe940e91bb14886134
mohammed EDDAHMANI <dsimo4323@gmail.com>
2024-07-30 13:45:26 +0200

    SOmm

--- commerce-sw/middleware.js
+++ commerce-sw/middleware.js
@@ -1,12 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export default function middleware(req) {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return NextResponse.redirect("/login");
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
